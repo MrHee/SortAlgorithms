424e00cede3558d0e665fc29a0391df8fb6f459a
Mr Hee <matteo.hee@spschools.org>
2023-03-15 08:20:22 -0600

    Let's try that again

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+int[] firstArray = {7,11,101,55,-4,100,32,2,27907200,16,117,-5,-250,19,72,-96};
+
+        sorting.displayArray(firstArray);
+
+        int[] sortedArray = sorting.bubbleSort(firstArray);
+
+        sorting.displayArray(sortedArray);
     }
 }
--- src/sorting.java
+++ src/sorting.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class sorting {
+public class sorting {
+
+    static int[] swap( int[] array, int i, int j){
+
+        int holder = array[i];
+        array[i] = array[j];
+        array[j] = holder;
+
+        return array;
+    }
+
+    static void displayArray(int[] array){
+
+        System.out.print("{ ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
+        System.out.println("}");
+
+    }
+
+
+    static int[] bubbleSort(int[] unsorted) {
+        boolean sorted = false;
+
+        while(!sorted){
+
+            sorted = true;
+        for (int i = 0; i < unsorted.length - 1; i++) {
+            if (unsorted[i] > unsorted[i + 1]) {
+                unsorted = swap(unsorted, i, i + 1);
+                sorted =false;
+
+            }
+        }
+    }
+
+
+
+            return unsorted;
+
+
+    }
+
+
 }
